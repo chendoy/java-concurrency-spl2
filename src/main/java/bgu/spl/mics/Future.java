154c3a3190679d6cd366968320324bf19dd7bfb3
chendoy <chendoy@post.bgu.ac.il>
2018-11-27 21:05:55 +0200

    implemented tests

--- src/main/java/bgu/spl/mics/Future.java
+++ src/main/java/bgu/spl/mics/Future.java
@@ -3,61 +3,63 @@ package bgu.spl.mics;
 import java.util.concurrent.TimeUnit;
 
 /**
- * A Future object represents a promised result - an object that will
- * eventually be resolved to hold a result of some operation. The class allows
- * Retrieving the result once it is available.
- * 
+ * A Future object represents a promised objectResult - an object that will
+ * eventually be resolved to hold a objectResult of some operation. The class allows
+ * Retrieving the objectResult once it is available.
+ *
  * Only private methods may be added to this class.
  * No public constructor is allowed except for the empty constructor.
  */
 public class Future<T> {
-	
+	private T objectResult;
+
 	/**
 	 * This should be the the only public constructor in this class.
 	 */
 	public Future() {
 		//TODO: implement this
+		objectResult =null;
 	}
-	
+
 	/**
-     * retrieves the result the Future object holds if it has been resolved.
-     * This is a blocking method! It waits for the computation in case it has
-     * not been completed.
-     * <p>
-     * @return return the result of type T if it is available, if not wait until it is available.
-     * 	       
-     */
+	 * retrieves the objectResult the Future object holds if it has been resolved.
+	 * This is a blocking method! It waits for the computation in case it has
+	 * not been completed.
+	 * <p>
+	 * @return return the objectResult of type T if it is available, if not wait until it is available.
+	 *
+	 */
 	public T get() {
 		//TODO: implement this.
 		return null;
 	}
-	
+
 	/**
-     * Resolves the result of this Future object.
-     */
+	 * Resolves the objectResult of this Future object.
+	 */
 	public void resolve (T result) {
 		//TODO: implement this.
 	}
-	
+
 	/**
-     * @return true if this object has been resolved, false otherwise
-     */
+	 * @return true if this object has been resolved, false otherwise
+	 */
 	public boolean isDone() {
 		//TODO: implement this.
 		return false;
 	}
-	
+
 	/**
-     * retrieves the result the Future object holds if it has been resolved,
-     * This method is non-blocking, it has a limited amount of time determined
-     * by {@code timeout}
-     * <p>
-     * @param timout 	the maximal amount of time units to wait for the result.
-     * @param unit		the {@link TimeUnit} time units to wait.
-     * @return return the result of type T if it is available, if not, 
-     * 	       wait for {@code timeout} TimeUnits {@code unit}. If time has
-     *         elapsed, return null.
-     */
+	 * retrieves the objectResult the Future object holds if it has been resolved,
+	 * This method is ndon-blocking, it has a limited amount of time determined
+	 * by {@code timeout}
+	 * <p>
+	 * @param timout 	the maximal amount of time units to wait for the objectResult.
+	 * @param unit		the {@link TimeUnit} time units to wait.
+	 * @return return the objectResult of type T if it is available, if not,
+	 * 	       wait for {@code timeout} TimeUnits {@code unit}. If time has
+	 *         elapsed, return null.
+	 */
 	public T get(long timeout, TimeUnit unit) {
 		//TODO: implement this.
 		return null;
